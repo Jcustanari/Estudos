c7528c756672e58ac29d350e2fdccaca1f3418d2
Jorge Custanari <jorge.custanari@outlook.com>
2020-01-19 21:27:24 -0300

    Alterações da Aula 2 do curso da alura.

--- Python/Jogo.py
+++ Python/Jogo.py
@@ -1,2 +1,17 @@
 
+print("#####################################")
 print("Bem vindo ao jogo de Adivinhação!")
+print("#####################################\n")
+
+numero_secreto = 42
+
+chute = int(input("Digite um numero: "))
+
+print("Voce digitou o numero: ", chute)
+
+if (numero_secreto == chute):
+    print("Voce acertou!!!")
+else:
+    print("Voce Errou!!!")
+
+print("Fim do Jogo!")
