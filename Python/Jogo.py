65b064ff21d8abeaaf060eb2f8a764acf3ae1337
Jorge Custanari <jorge.custanari@outlook.com>
2020-01-26 21:55:37 -0300

    Atualizações da aula 5 do curso de python

--- Python/Jogo.py
+++ Python/Jogo.py
@@ -5,15 +5,19 @@ print("#####################################\n")
 
 numero_secreto = 42
 tentativas = 3
-rodadas = 1
 
-while (rodadas <= tentativas):
-    print("Tentativas {} de {}".format(rodadas,tentativas))
-    chute = int(input("Digite um numero: "))
+
+for rodadas in range(0,tentativas,1):
+    print(f"Tentativas {rodadas+1} de {tentativas}") #.format(rodadas+1,tentativas))
+    chute = int(input("Digite um numero entre 1 e 100: "))
     print("\n")
 
+    if (chute < 1 or chute > 100):
+        print("Valor digitado tem que estar entre 1 e 100!")
+        continue
     if (numero_secreto == chute):
         print("Voce acertou!!!")
+        break
     else:
         if(chute > numero_secreto):
             print("Voce Errou! o seu chute foi maior que o numero secreto")
