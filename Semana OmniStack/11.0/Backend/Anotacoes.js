574639782f586789003280623cd33e7637aba49a
Jorge Custanari <jorge.custanari@outlook.com>
2020-03-29 19:58:05 -0300

    Semana OmniStack 11.0, Atualizações aula 2 - 01:01:00 horas.

---
+++ Semana OmniStack/11.0/Backend/Anotacoes.js
@@ -0,0 +1,19 @@
+
+
+/*******************************************************
+    Metodos HTTP:
+        GET: Buscar informações do Back-end
+        POST: Incluir informações no Back-end 
+        PUT: Alterar informações no Back-end 
+        DELETE: Deletar informações no Back-end
+    -------------------------------------------
+    Tipos de parametro:
+        Query Params: São parametros nomeados nas rotas para 
+            busca que é sinalizado por uma "?" seguido 
+            do nome do parametro e seu valor, 
+            Ex: /user?nome=jonas, utilizados para 
+            filtro e paginação
+        Route Params: Parametros utilizados para indentificar recursos,
+            ele é utilizado diretamente na rota, para definir o parametro
+        Request body: Corpo da requisição, utilizado para criar ou alterar recursos 
+*******************************************************/
