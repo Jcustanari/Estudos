f90fad77e3d69937dbdd68e0f4d48e3f992b0c3f
Jorge Custanari <jorge.custanari@outlook.com>
2020-05-12 19:23:20 -0300

    Organizando

---
+++ Python/Alura/Python 3/Forca.py
@@ -0,0 +1,19 @@
+
+def Jogar():
+   
+    print("#####################################")
+    print("#####Bem vindo ao jogo de Forca!#####")
+    print("#####################################\n")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+
+    print("Fim do jogo!")
+
+if(__name__ == "__main__"):
+    Jogar()
